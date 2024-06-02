29e83c958e7d0cd5add4cbc4d4faa29aed0a348b
Simon Hazard <simon.hazard@outlook.fr>
2024-06-02 22:47:24 +0200

    init websocket

--- back/go.mod
+++ back/go.mod
@@ -3,9 +3,9 @@ module bot
 go 1.22.3
 
 require (
-	github.com/bwmarrin/discordgo v0.28.1 // indirect
-	github.com/gorilla/websocket v1.5.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/bwmarrin/discordgo v0.28.1
+	github.com/gorilla/websocket v1.5.1
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
--- back/main.go
+++ back/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"bot/bot"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
+
+var wsUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var wsConn *websocket.Conn
+
+// var (
+// 	Token = flag.String("token", os.Getenv("BOT_TOKEN"), "Bot authentication token")
+// 	App   = flag.String("app", os.Getenv("APPLICATION_ID"), "Application ID")
+// 	Guild = flag.String("guild", os.Getenv("GUILD_ID"), "Guild ID")
+// )
 
 func main() {
 	err := godotenv.Load("local.env")
@@ -13,6 +33,64 @@ func main() {
 		log.Fatal("Error")
 	}
 	botToken := os.Getenv("BOT_TOKEN")
-	bot.BotToken = botToken
-	bot.Run()
+	// bot.BotToken = botToken
+	// bot.Run()
+
+	dg, err := discordgo.New("Bot " + botToken)
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+	defer dg.Close()
+
+	http.HandleFunc("/ws", handleConnections)
+	log.Println("http server started on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	log.Println("message", m.Attachments)
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	for _, attachments := range m.Attachments {
+		if attachments.ContentType == "image" {
+			sendMessageToWebSocket(attachments.URL)
+		}
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	var err error
+	wsConn, err = wsUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer wsConn.Close()
+
+	for {
+		_, _, err := wsConn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+	}
+}
+
+func sendMessageToWebSocket(message string) {
+	log.Println(message)
+	if wsConn != nil {
+		err := wsConn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			log.Println("write:", err)
+		}
+	}
 }
