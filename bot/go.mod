041ad65d4f7daac1be6a1600da3bf054374e31d3
Simon Hazard <simon.hazard@outlook.fr>
2024-06-09 17:24:37 +0200

    update bot to handle video and text

--- bot/go.mod
+++ bot/go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/bwmarrin/discordgo v0.28.1
 	github.com/gorilla/websocket v1.5.1
 	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
--- bot/main.go
+++ bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -19,7 +20,13 @@ var wsUpgrader = websocket.Upgrader{
 	},
 }
 
-var wsConn *websocket.Conn
+type Message struct {
+	Text      string   `json:"text"`
+	ImageURLs []string `json:"image_urls"`
+	VideoURLs []string `json:"video_urls"`
+}
+
+var websocketConnection *websocket.Conn
 
 // var (
 // 	Token = flag.String("token", os.Getenv("BOT_TOKEN"), "Bot authentication token")
@@ -28,25 +35,25 @@ var wsConn *websocket.Conn
 // )
 
 // Handle error
-func checkNilErr(e error, message string) {
-	if e != nil {
-		log.Fatal("Error message" + message)
+func checkNilErr(err error, message string) {
+	if err != nil {
+		log.Fatal(message, err)
 	}
 }
 
 func main() {
 	err := godotenv.Load("local.env")
-	checkNilErr(err, "env error")
+	checkNilErr(err, "env error:")
 
 	botToken := os.Getenv("BOT_TOKEN")
 
 	discord, discordErr := discordgo.New("Bot " + botToken)
-	checkNilErr(discordErr, "discord init error")
+	checkNilErr(discordErr, "discord init error:")
 
 	discord.AddHandler(messageCreate)
 
 	err = discord.Open()
-	checkNilErr(err, "discord open error")
+	checkNilErr(err, "discord open error:")
 
 	defer discord.Close()
 
@@ -58,28 +65,54 @@ func main() {
 	checkNilErr(err, "listen and server error")
 }
 
+// Create the message to send through the websocket
 func messageCreate(discord *discordgo.Session, message *discordgo.MessageCreate) {
 	if message.Author.ID == discord.State.User.ID {
 		return
 	}
 
+	var imageUrls []string
+	var videoUrls []string
+
+	// Check for attachments (video or image)
 	for _, attachments := range message.Attachments {
 		if strings.Contains(attachments.ContentType, "image") {
-			sendMessageToWebSocket(attachments.URL)
+			imageUrls = append(imageUrls, attachments.URL)
+		} else if strings.Contains(attachments.ContentType, "video") {
+			videoUrls = append(videoUrls, attachments.URL)
 		}
 	}
+
+	// Check for embed (video or image)
+	for _, embed := range message.Embeds {
+		if embed.Type == "image" {
+			imageUrls = append(imageUrls, embed.URL)
+		} else if embed.Type == "video" {
+			videoUrls = append(videoUrls, embed.URL)
+		}
+	}
+
+	messageToSend := Message{
+		Text:      message.Content,
+		ImageURLs: imageUrls,
+		VideoURLs: videoUrls,
+	}
+
+	sendMessageToWebSocket(messageToSend)
 }
 
+// Handle the connection of the websocket
 func handleConnections(w http.ResponseWriter, r *http.Request) {
 	var err error
-	wsConn, err = wsUpgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer wsConn.Close()
+
+	websocketConnection, err = wsUpgrader.Upgrade(w, r, nil)
+
+	checkNilErr(err, "ws connection:")
+
+	defer websocketConnection.Close()
 
 	for {
-		_, _, err := wsConn.ReadMessage()
+		_, _, err := websocketConnection.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
@@ -87,10 +120,19 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func sendMessageToWebSocket(message string) {
+// Send JSON message to our websocket
+func sendMessageToWebSocket(message Message) {
 	log.Println("message", message)
-	if wsConn != nil {
-		err := wsConn.WriteMessage(websocket.TextMessage, []byte(message))
+	if websocketConnection != nil {
+		messageJSON, err := json.Marshal(message)
+
+		if err != nil {
+			log.Println("error marshaling message:", err)
+			return
+		}
+
+		err = websocketConnection.WriteMessage(websocket.TextMessage, messageJSON)
+
 		if err != nil {
 			log.Println("write:", err)
 		}
