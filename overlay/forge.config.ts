cfabc371e05ccba681cba6d6e772d18431f326df
Simon Hazard <simon.hazard@outlook.fr>
2024-06-20 00:09:19 +0200

    bump version

--- overlay/forge.config.ts
+++ overlay/forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from "@electron/fuses";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    executableName: "memeover",
   },
   rebuildConfig: {},
   makers: [
