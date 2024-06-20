b93e3bc2c08c53142efce2a7004570b06ccae6bb
Simon Hazard <simon.hazard@outlook.fr>
2024-06-21 01:16:04 +0200

    update ui

--- overlay/src/ui/App.tsx
+++ overlay/src/ui/App.tsx
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const URL = "ws://localhost:8080/ws";
+
+type Message = {
+	text?: string | null;
+	url?: string | null;
+	isAnimated?: boolean | null;
+	code?: string;
+};
 
 const App = () => {
+	const [message, setMessage] = useState<Message>();
+	const [code, setCode] = useState<string>();
+
+	useEffect(() => {
+		let socket: WebSocket;
+
+		function createWebSocketConnection() {
+			socket = new WebSocket(URL);
+
+			socket.onmessage = (event) => {
+				const data: Message = JSON.parse(event.data);
+				setMessage(data);
+				setCode("");
+
+				if (data.code) {
+					setCode(data.code);
+				}
+			};
+
+			socket.onclose = (event) => {
+				console.log("WebSocket closed: ", event);
+			};
+
+			socket.onerror = (error) => {
+				console.log("WebSocket error: ", error);
+				socket.close();
+			};
+		}
+
+		// Initialize the first connection
+		createWebSocketConnection();
+
+		return () => {
+			if (socket) {
+				socket.close();
+			}
+		};
+	}, []);
+
 	return (
-		<div>
-			<div id="messages" />
-			<div id="messageCode" />
+		<div className="h-full w-full p-2 flex mx-auto justify-center items-center flex-col space-y-4">
+			<div id="flex justify-center items-center flex-col">
+				{message?.text ? <p className="text-center">{message.text}</p> : null}
+				{message?.url && !message?.isAnimated ? (
+					<img src={message.url} alt={message.url} />
+				) : null}
+				{message?.url && message?.isAnimated ? (
+					<video key={message.url} src={message.url} controls={false} autoPlay>
+						<track kind="captions" />
+					</video>
+				) : null}
+			</div>
+			{code ? (
+				<div className="text-black text-2xl flex justify-center items-center">
+					<p>{code}</p>
+				</div>
+			) : null}
 		</div>
 	);
 };
--- overlay/src/ui/index.tsx
+++ overlay/src/ui/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const container = document.getElementById("root") as HTMLDivElement;
-const root = createRoot(container);
-
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
